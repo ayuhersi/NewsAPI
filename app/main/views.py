31dd19470c1116d1b5389c062e5aabbea1526a3b
unknown <ayub7hersi@gmail.com>
2022-03-11 15:34:50 +0300

    adding news from news api

--- app/main/views.py
+++ app/main/views.py
@@ -6,10 +6,20 @@ from newsapi import NewsApiClient
 app = Flask(__name__)
 @app.route('/')
 def index():
+    newsapi = NewsApiClient(api_key='6c96ed5a47684e6aa3f87640d6945895')
+    top_headlines = newsapi.get_top_headlines(sources="bbc-news")
+    all_articles = newsapi.get_everything(sources="bbc-news")
     
-    '''
-    View root page function that returns the index page and its data
-    '''
+    t_articles = top_headlines['articles']
+    a_articles = all_articles['articles']
+    
+    news = []
+    desc = []
+    img = []
+    p_date = []
+    url = []
+    
+    
     
     return render_template('index.html')
 
@@ -17,4 +27,4 @@ if __name__ =='__main__':
     app.run(debug=True) 
 
 
-    
+
